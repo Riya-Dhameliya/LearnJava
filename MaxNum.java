cbcdda181a20645521a7b8f2c69f453c335864f9
Riya <42291916+Riya-Dhameliya@users.noreply.github.com>
2022-01-29 19:59:21 +0530

    Maximum Number

---
+++ MaxNum.java
@@ -0,0 +1,16 @@
+package Demo;
+import java.util.Scanner;
+public class MaxNum {
+	public static void main(String[] args) {
+		System.out.println("Please Enter 3 Numbers : ");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int d = c > (a > b ? a : b) ? c : ((a > b) ? a : b);
+        System. out. println("The Largest Number is :"+d);
+
+	}
+
+	}
+
